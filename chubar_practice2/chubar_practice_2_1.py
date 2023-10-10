7ff17a95689a6eef95985e08cd9bef468b22d6a2
“evgenii <jekaven2014@gmail.com>
2023-10-10 21:14:53 +0300

    fixes for initial practice

--- chubar_practice2/chubar_practice_2_1.py
+++ chubar_practice2/chubar_practice_2_1.py
@@ -38,13 +38,15 @@ class CEM(nn.Module):
         for trajectory in elite_trajectories:
             elite_states.extend(trajectory['states'])
             elite_actions.extend(trajectory['actions'])
+            if len(trajectory['states'])!=len(trajectory['actions']):
+                print(trajectory['states'],trajectory['actions'])
+        print(len(elite_states),len(elite_actions))
         elite_states = torch.FloatTensor(elite_states)
         elite_actions = torch.LongTensor(elite_actions)
-        
+        self.optimizer.zero_grad()
         loss = self.loss(self.forward(elite_states), elite_actions)
         loss.backward()
         self.optimizer.step()
-        self.optimizer.zero_grad()
         
         
 def get_trajectory(env, agent, trajectory_len, visualize=False):
@@ -53,7 +55,7 @@ def get_trajectory(env, agent, trajectory_len, visualize=False):
     state = env.reset()
     trajectory['states'].append(state)
     
-    for _ in range(trajectory_len):
+    for i in range(trajectory_len):
         
         action = agent.get_action(state)
         trajectory['actions'].append(action)
@@ -66,15 +68,20 @@ def get_trajectory(env, agent, trajectory_len, visualize=False):
             
         if visualize:
             env.render()
-            
-        trajectory['states'].append(state)
+            print(i)
+        
+        if(i != trajectory_len-1):
+            trajectory['states'].append(state)
             
     return trajectory
 
 def get_elite_trajectories(trajectories, q_param):
     total_rewards = [trajectory['total_reward'] for trajectory in trajectories]
     quantile = np.quantile(total_rewards, q=q_param) 
-    return [trajectory for trajectory in trajectories if trajectory['total_reward'] > quantile]
+    print(quantile, np.max(total_rewards), len(total_rewards), np.min(total_rewards))
+    if(quantile == np.max(total_rewards)):
+        print("hi", np.sort(total_rewards))
+    return [trajectory for trajectory in trajectories if trajectory['total_reward'] >= quantile]
 
 
 env = gym.make('CartPole-v1')
@@ -83,8 +90,8 @@ action_n = 2
 
 agent = CEM(state_dim, action_n)
 episode_n = 50
-trajectory_n = 20
-trajectory_len = 500
+trajectory_n = 200
+trajectory_len = 210
 q_param = 0.8
 
 for episode in range(episode_n):
@@ -92,7 +99,8 @@ for episode in range(episode_n):
     
     mean_total_reward = np.mean([trajectory['total_reward'] for trajectory in trajectories])
     print(f'episode: {episode}, mean_total_reward = {mean_total_reward}')
-    
+    if(mean_total_reward>0.95*trajectory_len):
+        break
     elite_trajectories = get_elite_trajectories(trajectories, q_param)
     
     if len(elite_trajectories) > 0:
